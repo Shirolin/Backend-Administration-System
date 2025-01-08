7f3f14335e15cd0a9ae8930ec9ed370adc434443
Shirolin <shirolin.com@gmail.com>
2025-01-08 22:17:53 +0800

    ✨ feat(config): 更新文件系统配置，修改admin驱动的根路径和URL

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,9 +57,9 @@ return [
 
         'admin' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . '/storage',
+            'root' => public_path('uploads'),
             'visibility' => 'public',
+            'url' => env('APP_URL') . '/uploads',
         ],
 
         's3' => [
