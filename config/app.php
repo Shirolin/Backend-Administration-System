de685cb5bb2f18c3ae41a9eefaaf023ed275a24f
Shirolin <shirolin.com@gmail.com>
2025-01-08 21:28:02 +0800

    ✨ feat(provider): 添加DataBaseQueryServiceProvider以记录SQL查询日志

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\DataBaseQueryServiceProvider::class, // SQL记录日志
 
     ],
 
